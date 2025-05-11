bf9ab048ccd3b65c6fb1f13a0ed698f7d7bb7427
YMT 114514 <chiakiyamato5@gmail.com>
2025-05-11 11:33:47 +0900

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,18 @@
+# reversi！
+BOARD_SIZE = 8
+
+def create_board():
+    board = [["." for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
+    mid = BOARD_SIZE // 2
+    board[mid - 1][mid - 1] = "W"
+    board[mid][mid] = "W"
+    board[mid - 1][mid] = "B"
+    board[mid][mid - 1] = "B"
+    return board
+
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
+
+board = create_board()
+print_board(board)
